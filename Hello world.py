cf15c1c9676d120363dddfa59598e58f0b294e82
leesangwoo2025 <leesangu0418@gmail.com>
2025-10-17 23:51:29 +0900

    Update Hello world.py

--- Hello world.py
+++ Hello world.py
@@ -1,7 +1,11 @@
 def print_hello():
     print("Hello world!")
 
+def print_KIT():
+    print("Kumoh National Institute of Technolgy.")
+
 if __name__ == '__main__':
+    print_KIT()
     print_hello()
 
     
